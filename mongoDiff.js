f19636ce742142bc17af96747eb9f5c7fd08d3e6
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2018-10-20 07:39:03 +0200

    mongoDiff sub model

--- mongoDiff.js
+++ mongoDiff.js
@@ -1,25 +1,39 @@
-export default (before, after) => {
+const getSetUnset = (before, after) => {
   const $set = {}
-  let shouldIncludeSet = false
   const $unset = {}
-  let shouldIncludeUnset = false
   for (const [key, value] of Object.entries(after)) {
     if (value !== undefined && before[key] !== value) {
-      $set[key] = value
-      shouldIncludeSet = true
+      if (value instanceof Object && before[key] instanceof Object) {
+        const { $set: nestedSet, $unset: nestedUnset } = getSetUnset(
+          before[key],
+          after[key]
+        )
+        for (const [nestedKey, nestedValue] of Object.entries(nestedSet)) {
+          $set[`${key}.${nestedKey}`] = nestedValue
+        }
+        for (const [nestedKey, nestedValue] of Object.entries(nestedUnset)) {
+          $unset[`${key}.${nestedKey}`] = nestedValue
+        }
+      } else {
+        $set[key] = value
+      }
     }
   }
   for (const [key, value] of Object.entries(before)) {
     if (value !== undefined && after[key] === undefined) {
       $unset[key] = ""
-      shouldIncludeUnset = true
     }
   }
+  return { $set, $unset }
+}
+
+export default (before, after) => {
+  const { $set, $unset } = getSetUnset(before, after)
   const results = {}
-  if (shouldIncludeSet) {
+  if (Object.keys($set).length > 0) {
     results.$set = $set
   }
-  if (shouldIncludeUnset) {
+  if (Object.keys($unset).length > 0) {
     results.$unset = $unset
   }
   return results
--- mongoDiff.test.js
+++ mongoDiff.test.js
@@ -21,3 +21,31 @@ it("unsets if missing property", () => {
   })
   expect(mongoDiff({ foo: undefined }, {})).toEqual({})
 })
+
+describe("sub model", () => {
+  it("creates object", () => {
+    expect(mongoDiff({}, { foo: {} })).toEqual({ $set: { foo: {} } })
+  })
+
+  it("deletes object", () => {
+    expect(mongoDiff({ foo: {} }, {})).toEqual({ $unset: { foo: "" } })
+  })
+
+  it("sets new properties", () => {
+    expect(mongoDiff({ foo: {} }, { foo: { bar: 2 } })).toEqual({
+      $set: { "foo.bar": 2 },
+    })
+  })
+
+  it("updates new properties", () => {
+    expect(mongoDiff({ foo: { bar: 1 } }, { foo: { bar: 2 } })).toEqual({
+      $set: { "foo.bar": 2 },
+    })
+  })
+
+  it("removes old properties", () => {
+    expect(mongoDiff({ foo: { bar: 1 } }, { foo: {} })).toEqual({
+      $unset: { "foo.bar": "" },
+    })
+  })
+})
