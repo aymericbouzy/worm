5058f25281417928807af1909fb47fdd14e85089
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2018-10-20 07:47:17 +0200

    mongodiff arrays

--- mongoDiff.test.js
+++ mongoDiff.test.js
@@ -22,9 +22,17 @@ it("unsets if missing property", () => {
   expect(mongoDiff({ foo: undefined }, {})).toEqual({})
 })
 
+it("works with dates", () => {
+  const date = new Date()
+  expect(mongoDiff({}, { foo: date })).toEqual({ $set: { foo: date } })
+})
+
 describe("sub model", () => {
   it("creates object", () => {
     expect(mongoDiff({}, { foo: {} })).toEqual({ $set: { foo: {} } })
+    expect(mongoDiff({}, { foo: { bar: 1 } })).toEqual({
+      $set: { foo: { bar: 1 } },
+    })
   })
 
   it("deletes object", () => {
@@ -46,6 +54,46 @@ describe("sub model", () => {
   it("removes old properties", () => {
     expect(mongoDiff({ foo: { bar: 1 } }, { foo: {} })).toEqual({
       $unset: { "foo.bar": "" },
+    })
+  })
+})
+
+describe("array", () => {
+  it("sets array", () => {
+    expect(mongoDiff({}, { foo: [] })).toEqual({ $set: { foo: [] } })
+    expect(mongoDiff({}, { foo: ["bar"] })).toEqual({ $set: { foo: ["bar"] } })
+    expect(mongoDiff({}, { foo: [{ bar: 1 }] })).toEqual({
+      $set: { foo: [{ bar: 1 }] },
+    })
+  })
+
+  it("adds element", () => {
+    expect(mongoDiff({ foo: [] }, { foo: ["bar"] })).toEqual({
+      $set: { "foo.0": "bar" },
+    })
+    expect(mongoDiff({ foo: ["bar"] }, { foo: ["bar", "baz"] })).toEqual({
+      $set: { "foo.1": "baz" },
+    })
+    expect(mongoDiff({ foo: [] }, { foo: [{ bar: 1 }] })).toEqual({
+      $set: { "foo.0": { bar: 1 } },
+    })
+  })
+
+  it("updates element", () => {
+    expect(mongoDiff({ foo: ["bar"] }, { foo: ["baz"] })).toEqual({
+      $set: { "foo.0": "baz" },
+    })
+    expect(mongoDiff({ foo: [{ bar: 1 }] }, { foo: [{ bar: 2 }] })).toEqual({
+      $set: { "foo.0.bar": 2 },
+    })
+    expect(mongoDiff({ foo: [{ bar: 1 }] }, { foo: [{}] })).toEqual({
+      $unset: { "foo.0.bar": "" },
+    })
+  })
+
+  it("removes element", () => {
+    expect(mongoDiff({ foo: [{ bar: 1 }] }, { foo: [] })).toEqual({
+      $unset: { "foo.0": "" },
     })
   })
 })
